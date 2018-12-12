cac05aa2c3f04f4b1324e3e59e15ba2b0a501862
Zachary Chandler <mr.zachchandler@gmail.com>
2018-12-11 19:51:24 -0500

    Initial Commit

---
+++ FirebaseNotifications/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FirebaseNotifications
+//
+//  Created by Zachary Chandler on 12/11/18.
+//  Copyright © 2018 Zachary Chandler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
